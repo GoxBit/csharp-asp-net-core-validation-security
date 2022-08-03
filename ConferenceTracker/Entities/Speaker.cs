b8e1afe686a747f83fa796943c6b2752aabc56e5
Luis Duran <goxbitdev@gmail.com>
2022-08-03 11:41:23 -0500

    properties validations

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,16 +6,34 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Text)]
         [Display(Name = "First name")]
+        [Required]
         public string FirstName { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Text)]
         [Display(Name = "Last name")]
+        [Required]
         public string LastName { get; set; }
+
+        [StringLength(500, MinimumLength = 10)]
+        [DataType(DataType.MultilineText)]
+        [Required]
         public string Description { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         public bool IsStaff { get; set; }
     }
 }
